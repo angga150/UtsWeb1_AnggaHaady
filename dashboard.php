ed30762896ed69dbc3f37072b67798d7167d65a1
angga150 <wijaya150906@gmail.com>
2025-11-06 10:02:23 +0700

    dashboard penjualan

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,15 @@ if( !isset($_SESSION['username']) ) {
 }
 ?>
 
+<?php
+$nama_barang = ["Semangka", "Nanas", "Pisang", "Alpukat", "Jeruk",];
+$harga_barang = [35000, 25000, 20000, 21000, 22000];
+
+echo "<h2>-- Polgan Mart --</h2>";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
