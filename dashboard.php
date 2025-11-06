3ecb903cb179e3868a04b79c7ecadf2e8e2d07d3
angga150 <wijaya150906@gmail.com>
2025-11-06 10:06:57 +0700

    output akhir

--- dashboard.php
+++ dashboard.php
@@ -31,6 +31,23 @@ foreach ($pilih_index as $idx) {
     $jumlah[] = rand(1, 5); 
 }
 
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Nama Barang</th><th>Harga</th><th>Jumlah</th><th>Total</th></tr>";
+
+foreach ($beli as $i => $barang) {
+    $sub_total = $harga_barang[array_search($barang, $nama_barang)] * $jumlah[$i];
+    echo "<tr>
+            <td>{$barang}</td>
+            <td>Rp " . number_format($harga_barang[array_search($barang, $nama_barang)], 0, ',', '.') . "</td>
+            <td>{$jumlah[$i]}</td>
+            <td>Rp " . number_format($sub_total, 0, ',', '.') . "</td>
+          </tr>";
+    $grandtotal += $sub_total;
+}
+
+echo "<tr><td colspan='3'><b>Total Belanja</b></td>
+      <td><b>Rp " . number_format($grandtotal, 0, ',', '.') . "</b></td></tr>";
+echo "</table>";
 
 ?>
 
